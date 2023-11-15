c7584e72802433240498d03225687562e53eba72
Promise Igbo <100773978+Promise-Igbo@users.noreply.github.com>
2023-11-15 12:53:31 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@
   apps: [
     {
       name: "radix-ui",
-      script: "./node_modules/.bin/next",
+      script: "./node_modules/next/dist/bin/next",
       args: "start -p " + (process.env.PORT || 3000),
       watch: false,
       autorestart: true,
