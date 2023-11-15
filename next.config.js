8acfd0b8de8f4c93d9bce1a7335c538b065e480c
Promise Igbo <100773978+Promise-Igbo@users.noreply.github.com>
2023-11-15 11:59:05 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,3 +13,15 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+module.exports = {
+  apps: [
+    {
+      name: "radix-ui",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start -p " + (process.env.PORT || 3000),
+      watch: false,
+      autorestart: true,
+    },
+  ],
+};
+
